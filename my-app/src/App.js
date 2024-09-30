daad87d1c159057fedff61a49d756248afc991e3
DESKTOP-6QDL7JM\Dawid Turczynski 3tp <dawido.tur@gmail.com>
2024-09-30 13:16:58 +0200

    instalacja bootstrap, utworzono funkcję onSubmit, dodanie formularzy

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.css";
 import './App.css';
 
 function App() {
+  
+  function onSubmit(event) {
+    event.preventDefault();
+
+    const tytul = event.target.elements.tytul.value;
+    const rodzaj = event.target.elements.rodzaj.value;
+    console.log(`tytul: ${tytul}, rodzaj: ${rodzaj}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <form onSubmit={onSubmit}>
+        <div className="col-6">
+          <label htmlFor="tytul" className="form-label">Tytuł filmu</label>
+          <input type="text" className="form-control" id="tytul" placeholder="" />
+        </div>
+        <div className="col-6">
+        <label htmlFor="rodzaj" className="form-label">Rodzaj</label>
+        <select id="rodzaj" className="form-select">
+          <option defaultValue={0}></option>
+          <option value="1">Komedia</option>
+          <option value="2">Obyczajowy</option>
+          <option value="3">Sensacyjny</option>
+          <option value="4">Horror</option>
+        </select>  
+        </div><br></br>
+          <button type="submit" className="btn btn-primary">
+            Dodaj
+          </button>
+        </form>
     </div>
   );
 }
